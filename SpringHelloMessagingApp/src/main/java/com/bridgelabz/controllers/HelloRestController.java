09460969a1195dfe9aaea4b417a09cfb8a820777
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-30 19:13:46 +0530

    Added: Make Rest Calls by using Get Request passing name as Query parameter

--- SpringHelloMessagingApp/src/main/java/com/bridgelabz/controllers/HelloRestController.java
+++ SpringHelloMessagingApp/src/main/java/com/bridgelabz/controllers/HelloRestController.java
@@ -1,13 +1,29 @@
 package com.bridgelabz.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestController {
 
+    // UC1 : Create a Rest Controller to demonstrate
+    //       the various HTTP Methods and respond
+    //       hello messages to the User.
     @RequestMapping(value = "/hello")
     public String sayHello() {
-        return "Hello From BridgeLabz!!! ";
+        return "Hello From BridgeLabz ";
+    }
+
+    // UC2 : Use GET Request Method and pass name
+    //       as query parameter.
+    // By Using @RequestMapping
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!!!";
+    }
+
+    // By Using @GetMapping
+    @GetMapping({"/query1"})
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!!!";
     }
 }
